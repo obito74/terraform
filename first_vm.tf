47da3b307b1e7e6d3818c209ffeb93fb5c5d9263
Brett Gerhardt <brett23g@gmail.com>
2023-06-26 17:49:23 -0400

    updated first commit

--- first_vm.tf
+++ first_vm.tf
@@ -1,15 +1,11 @@
 provider "aws" {
-
-    region = "us-east-1"
-    
-
+  profile = "default"
 }
-
 resource "aws_instance" "myec2" {
     ami = "ami-022e1a32d3f742bd8"
     instance_type = "t2.micro"
 
     tags = {
-      Name = "my-first-ec2"
+      Name = "my-first-ec2-instance"
   }
 }
